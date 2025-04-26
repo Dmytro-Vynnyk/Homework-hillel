f1b31c11f747d3ce78f721d385067047af14ab84
Dmytro-Vynnyk <dafsauy8@gmail.com>
2025-04-26 22:35:14 +0300

    email filtering by domains

---
+++ Paranoia.js
@@ -0,0 +1,28 @@
+var arr = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com"
+    },
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        email:"dmitro.porohov@yahoo.com"
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru"
+    },
+];
+var trustedEmails = [];
+var emailRegex = /^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@(gmail\.com|yahoo\.com)$/;
+for (var i = 0; i < arr.length; i++) {
+    var email = arr[i].email;
+    if (emailRegex.test(email)) {
+        trustedEmails.push(email);
+    }
+}
+
+console.log(trustedEmails);
+
